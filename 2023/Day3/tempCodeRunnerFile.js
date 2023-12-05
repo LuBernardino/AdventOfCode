de91e639f401688059c37ab6af3bf8f0e8bd7b7f
Luana Bernardino <luanabernardino2009@hotmail.com>
2023-12-05 20:28:59 +0000

    day 4

--- 2023/Day3/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-numberList.forEach((x) => {
-        console.log(x.number, x.positions, x.positions.some((position) => checkAdjacentSymbol(matrix, position.i, position.j, width, height)));
-    });
--- 2023/Day4/solution.js
+++ 2023/Day4/solution.js
@@ -1,26 +1,39 @@
 // --- Day 4: Scratchcards ---
 const { input } = require("./input.js");
-let total = 0;
-input.split("\n").forEach((line, lineIndex) => {
-    const numerosString = line.split(":")[1].trim().replace(/[|\s]+/g, ',');
-    const numerosArray = numerosString.split(',').map(Number);
-    console.log(numerosArray);
-    const contagemNumeros = {};
+let total1 = 0;
 
-    numerosArray.forEach((numero) => {
-        contagemNumeros[numero] = (contagemNumeros[numero] || 0) + 1;
-    });
-    
-    const quantidadeParesRepetidos = Object.values(contagemNumeros).reduce((total, contagem) => {
-        return total + Math.floor(contagem / 2);
-    }, 0);
-    
-    if(quantidadeParesRepetidos > 0)
-    {
-        total += 2 ** (quantidadeParesRepetidos - 1);
-    }
-        
-});
+let inputs = input.split("\n");
+let instances = Array.from({ length: inputs.length }, () => 1);
 
-console.log(`Result part 1: ${total}`);
- 
+for (let lineIndex = 0; lineIndex < inputs.length; lineIndex++) {
+  const line = inputs[lineIndex];
+  const [cardId, cardsValues] = line.split(":");
+  let [_, id] = cardId.split("Card");
+  id = Number(id.trim());
+  const cardsStringsValues = cardsValues.trim().replace(/[|\s]+/g, ",");
+  const cardsNumberValues = cardsStringsValues.split(",").map(Number);
+  const numbersCount = {};
+
+  cardsNumberValues.forEach((value) => {
+    numbersCount[value] = (numbersCount[value] || 0) + 1;
+  });
+
+  const quantityRepeatedPairs = Object.values(numbersCount).reduce(
+    (total, count) => {
+      return total + Math.floor(count / 2);
+    },
+    0
+  );
+
+  if (quantityRepeatedPairs > 0) {
+    if (lineIndex < inputs.length) total1 += 2 ** (quantityRepeatedPairs - 1);
+  }
+
+  for (let i = 0; i < quantityRepeatedPairs; i++) {
+    if (id + i < instances.length)
+      instances[id + i] = instances[id + i] + instances[id - 1];
+  }
+}
+
+const total2 = instances.reduce((total, instance) => total + instance, 0);
+console.log(`Result part 1: ${total1} | Result part 2: ${total2}`);
